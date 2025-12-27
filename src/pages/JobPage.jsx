6a5401b27bb7b66a358abfe8c438ff5fcf9bde7d
Ogbodo Ebuka <ogbodoebuka2@gmail.com>
2025-12-27 16:15:34 +0100

    removed job successfully

--- src/pages/JobPage.jsx
+++ src/pages/JobPage.jsx
@@ -1,8 +1,23 @@
-import { useParams, useLoaderData, Link } from 'react-router-dom';
+import { useParams, useLoaderData, Link, useNavigate } from 'react-router-dom';
 import { FaMapMarker } from 'react-icons/fa';
-const JobPage = () => {
+import { toast } from 'react-toastify'
+
+const JobPage = ({ deleteJob }) => {
     const { id } = useParams();
     const job = useLoaderData();
+    const navigate = useNavigate();
+
+    const OnDeleteClick = (jobId) => {
+        const confirm = window.confirm('Are you sure you want to delete this listing?')
+
+        if (!confirm) return;
+
+        deleteJob(jobId);
+
+        toast.success('Job deleted successfully')
+
+        navigate('/jobs');
+    }
     
   return (
     <>
@@ -82,6 +97,7 @@ const JobPage = () => {
                 >Edit Job
               </Link>
               <button
+                onClick={() => OnDeleteClick(job.id)}
                 className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
               >
                 Delete Job
